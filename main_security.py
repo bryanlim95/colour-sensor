7c264ba530c4fff52c6704be6b1adc09c3786fee
bryanlim95 <60686136+bryanlim95@users.noreply.github.com>
2020-11-17 19:14:10 +0800

    Update main_security.py

--- main_security.py
+++ main_security.py
@@ -1,9 +1,16 @@
 from tkinter import Tk, Frame, Label, Entry, Button, RIGHT, TOP, YES, X
 import tkinter.messagebox as box
-import random
 import time
+import math
+import encryption
+import trigger2
+import threading
 
-# Declaring Classes
+dig = []
+digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+alert_dist = 30;
+
+
 class Clock:
     def __init__(self):
         self.time1 = time.strftime('%H:%M:%S')
@@ -17,59 +24,54 @@ class Clock:
         self.time1 = time.strftime('%H:%M:%S')
         self.watch.configure(text=self.time1)
         self.mFrame.after(200, self.changeLabel) #it'll call itself continuously
-
-# Encryption Algorithm 
-def des_encrypt(realText, step):
-    outText = []
-    uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    for eachChar in realText:
-        if eachChar in uppercase:
-            index = uppercase.index(eachChar)
-            crypting = (index + step) % 26
-            newChar = uppercase[crypting]
-            outText.append(newChar)
-
-        elif eachChar in  digits:
-            index = digits.index(eachChar)
-            crypting = (index - step) % 10
-            newChar = digits[crypting]
-            outText.append(newChar)
-
-        else:
-            newChar = eachChar
-            outText.append(newChar)
-
-    random.shuffle(outText)
-
-    return outText
-
-step = 3
-
-#Authentication
-
-dig = []
-digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
+'''
+class TempSensor:
+    def __init__(self):
+        (humidity,temp) = temp_sensor.temp_call()
+        self.temp1 = round(temp,2)
+        self.mFrame = Frame()
+        self.mFrame.pack(side=TOP,expand=YES,fill=X)
+        self.watch = Label(self.mFrame, text=self.temp1, font=('times',12,'bold'))
+        self.watch.pack()
+        self.changeLabel2() #first call it manually
+        
+    def changeLabel2(self):
+        (humidity,temp) = temp_sensor.temp_call()
+        self.temp1 = round(temp,2)
+        self.mFrame = Frame()
+        self.temp = temp_sensor.temp_call()
+        self.watch.configure(text=self.temp1)
+        self.mFrame.after(1000, self.changeLabel2) #it'll call itself continuously
+'''
+        
+class Threading(object):
+    def __init__(self, interval=2):
+        self.interval = interval
+        thread = threading.Thread(target=self.u_sensors, args=())
+        thread.daemon = True
+        thread.start()
+        
+    def u_sensors(self):
+        while True:
+            intrude = trigger2.polling()
+            print(intrude)
+            if intrude < alert_dist:
+                if intrude > 0:          
+                    trigger2.intruder_alert();                   
+            time.sleep(self.interval)
 
 def dialog1():
     message = entry1.get()
-    translated = des_encrypt(message, step)
+    translated = encryption.des_encrypt(message, 3)
     for eachChar in translated:
         if eachChar in digits:
-            dig.append((int(eachChar)+step)%10)
+            dig.append((int(eachChar)+3)%10)
     if (sum(dig) > 15):
         box.showinfo('info','Access Granted')
-        #response = 'Access Granted'
-        #time.sleep(2)
+        tr = Threading()
         window.destroy()
     else:
         box.showinfo('info','Access Denied')
-        #response = 'Access Denied'
-        #time.sleep(2)
-        window.destroy()
-
-# Designing the GUI
 
 window = Tk()
 window.title('Access Code')
@@ -87,8 +89,12 @@ Label2.pack(padx = 15,pady = 5)
 
 obj1 = Clock()
 
-Label3 = Label(window,text = 'Temp:')
+Label3 = Label(window,text = 'Temp(C)')
 Label3.pack(padx = 15,pady = 5)
+
+#trigger2.main()
+
+#obj2 = TempSensor()
 
 btn.pack(side = RIGHT , padx = 5)
 frame.pack(padx = 100,pady = 19)
